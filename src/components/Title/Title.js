017c899736db2ee078cb3f193d43ae0a10e09932
Mahmut ÖZ <mahmutoz8197@gmail.com>
2022-11-11 11:51:51 +0300

    Feat: add Title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "src/components/Title/Title.module.scss";
+import cx from "classnames";
+
+const Title = ({ as = "h2", className, children, ...props }) => {
+  const cName = Object.keys(styles)
+    .filter((cn) => className.includes(cn))
+    .map((key) => styles[key]);
+
+  return React.createElement(as, { className: cx(...cName), ...props }, children);
+};
+
+export default Title;
+
+Title.propTypes = {
+  as: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
